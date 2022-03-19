72d419da94fb786679d56a34fde957d7190636f0
EverthM <machadoeverth2015@gmail.com>
2022-03-19 00:29:59 -0600

    vistas

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -7,15 +7,15 @@ use Illuminate\Http\Request;
 class CursoController extends Controller
 {
     public function index(){
-        return "Pagina Principal" ;
+        return view('cursos.index');
     }
 
 public function create() {
-    return "Bienvenido a crear un curso" ;
+    return view('cursos.create') ;
 }
  public function show($curso){
 
-    return "Bienvenido al curso $curso" ;
+    return view('cursos.show',['curso'=>$curso]);
  }
 
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function __invoke(){
-        return "Bienvenido a la pagina de chistes para la lilyta mi amor guapaaaaaa";
+
+        return view('home');
     }
 }
